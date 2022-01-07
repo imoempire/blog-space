ee6b57f6fd1c3af97506ac6120c7e6553557ba6f
imoempire <mrimoempire@gmail.com>
2022-01-07 16:01:57 +0000

    add icons and empty state

--- src/FirebaseState/Reducer/Reducer.js
+++ src/FirebaseState/Reducer/Reducer.js
@@ -9,42 +9,7 @@ const ADD_STORY = "ADD_STORY";
 
 const initialState = {
    stories : [
-      {
-         id: 1,
-         title: '7 CSS tools you should be using ',
-         category: 'development',
-         subCategory: ['frontend', 'ui/ux', 'design'],
-         description:
-           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-         authorName: 'John Doe',
-         authorAvatar: imageAvatar,
-         createdAt: 'June 03, 2021',
-         cover: image,
-       },
-       {
-         id: 2,
-         title: 'Milan Places That Highlight The City',
-         category: 'travel',
-         subCategory: ['vacation', 'holidays', 'sight seeing'],
-         description:
-           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-         authorName: 'John Doe',
-         authorAvatar: imageAvatar,
-         createdAt: 'June 03, 2021',
-         cover: image,
-       },
-       {
-         id: 3,
-         title: 'Online Shopping – An Alternative to Shopping in the Mall',
-         category: 'shopping',
-         subCategory: ['e-commerce store', 'clothing', 'shopping store'],
-         description:
-           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-         authorName: 'John Doe',
-         authorAvatar: imageAvatar,
-         createdAt: 'June 03, 2021',
-         cover: image,
-       },
+     
    ],
 };
 
